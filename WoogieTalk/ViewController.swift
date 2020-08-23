f81c923193bf9d0081a8618732e8a08efef028d0
yunuki <woogie_ing@naver.com>
2020-08-23 18:46:30 +0900

    Initial Commit

---
+++ WoogieTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WoogieTalk
+//
+//  Created by woogie on 2020/08/23.
+//  Copyright © 2020 woogie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
